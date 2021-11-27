cf926a5e925b9366b00af75cd69bdfc81010bf10
William Antonio <will.ntn@gmail.com>
2021-11-27 10:25:20 -0300

    times 1 a 3

--- time2.py
+++ time2.py
@@ -1,8 +1,13 @@
 import respostas
+import constant
 
 def proximoPasso(direcao, posicao, aumenta, diminui):
 
     # IMPLEMENTAR FUNCAO QUE CALCULA O PROXIMO PASSO DA COBRA
+    if direcao == aumenta:
+        return posicao + 1
+    elif direcao == diminui:
+        return posicao - 1
 
     return respostas.proximoPasso(direcao,posicao,aumenta,diminui)
 
@@ -10,5 +15,10 @@ def proximoPasso(direcao, posicao, aumenta, diminui):
 def bateuNasBordas(x, y):
 
     # IMPLEMENTAR FUNCAO QUE VERIFICA SE BATEMOS NAS BORDAS
+    if x == 0 or x == constant.LARGURA_JANELA-1:
+        return True
+    if y == 0 or y == constant.ALTURA_JANELA-1:
+        return True
+    return False
 
     return respostas.bateuNasBordas(x,y)
--- time3.py
+++ time3.py
@@ -1,18 +1,51 @@
+from random import randint, random
 import respostas
+import constant
 
 # retorna nova posicao x,y da nova fruta
 def novaFruta(janela, cobra):
-
     # IMPLEMENTAR FUNCAO QUE CRIA NOVA FRUTA E IMPRIME NA TELA
+    fruta = ()
 
-    return respostas.novaFruta(janela, cobra)
+    while fruta == ():
+        # criar uma posicao aleatoria
+        x = randint(1, constant.ALTURA_JANELA-2)
+        y = randint(1, constant.LARGURA_JANELA-2)
+
+        fruta = (x, y)
+
+        # verificar se a fruta nao esta em cima da cobra
+        if fruta in cobra:
+            fruta = ()
+
+    janela.addch(fruta[0], fruta[1], constant.APPLE)
+
+    return fruta
 
 
 # retorna cobra atualizada, posicao da nova fruta
 # se necessario e pontos atualizados
 # atualiza posicao da cobra e fruta na tela se necessario
-def daProximoPasso(janela,cobra,fruta, pontos):
+def daProximoPasso(janela, cobra, fruta, pontos):
 
     # IMPLEMENTAR FUNCAO QUE DA PROXIMO PASSO
-   
-    return respostas.daProximoPasso(janela,cobra,fruta,pontos)
+    if cobra[0] == fruta:
+        # adicionar na tela mais um pedaco da cobra (cabeca)
+        janela.addch(cobra[0][0], cobra[0][1], constant.SNAKE)
+
+        # adicionar outra fruta
+        fruta = novaFruta(janela, cobra)
+
+        # adicionar um ponto
+        pontos = pontos + 1
+    else:
+        # adiciona na tela mais um pedaco da cobra (cabeca)
+        janela.addch(cobra[0][0], cobra[0][1], constant.SNAKE)
+
+        # tira ultimo elemento da lista
+        rabo = cobra.pop()
+
+        # remove da tela o rabo da cobra
+        janela.addch(rabo[0], rabo[1], " ")
+
+    return cobra, fruta, pontos
